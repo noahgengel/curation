e9d8e5df44ea23bb24a01e2df2291e9976c02d53
Noah Engel <ne2310@cumc.columbia.edu>
2020-03-24 15:00:26 -0400

    Improved docstrings. Changed .gitignore to prevent files used for dataframe generation from making their way into any commits.

--- data_steward/analytics/tools/create_dq_issue_site_dfs.py
+++ data_steward/analytics/tools/create_dq_issue_site_dfs.py
@@ -1,4 +1,16 @@
 """
-File is intended to 
+File is intended to create dataframes for each of the sites to better catalog
+their respective data quality issues. The output of this file should
+be useful for uploading to a project management tool that could then be
+provisioned to the different sites.
 
+This project management tool could then, in turn, enable sites to more easily
+identify their data quality issues and allow the DRC to more easily track
+HPO engagement.
+
+For a full description of this issue, please see EDQ-427.
+
+Start Date: 03/24/2020 (v1)
 """
+
+
