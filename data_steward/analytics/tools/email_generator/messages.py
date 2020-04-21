ea328cd5623be0bd93a0d3ffc406510b6df932be
Noah Engel <ne2310@cumc.columbia.edu>
2020-04-21 11:50:17 -0400

    Essentially started to appropriate the metrics_over_time library attributes for the email_generator.

---
+++ data_steward/analytics/tools/email_generator/messages.py
@@ -0,0 +1,12 @@
+"""
+File is intended to store some of the messages to be put into the program.
+These messages can include:
+    - Error messages
+    - Text for the introduction/conclusion of the email
+"""
+
+fnf_error = \
+    "{file} not found in the current directory: {cwd}. " \
+    "Please ensure that the file names are " \
+    "consistent between the Python script and the " \
+    "file name in your current directory."
